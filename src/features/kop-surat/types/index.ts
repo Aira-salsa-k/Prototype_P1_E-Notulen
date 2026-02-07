ab61d64560641ee32c0335695625fa54da9d00ee
Aira-salsa-k <airasalsakusuma@gmail.com>
2026-02-07 18:00:58 +0900

    resolve bug :d[

--- src/features/kop-surat/types/index.ts
+++ src/features/kop-surat/types/index.ts
@@ -2,7 +2,7 @@ export interface KopSuratConfig {
   logoUrl: string;
   institutionName: string; // e.g. DEWAN PERWAKILAN RAKYAT DAERAH
   districtName: string; // e.g. KABUPATEN KEEROM
-  address: string; // e.g. Jl. Trans Papua No. ....d
+  address: string; // e.g. Jl. Trans Papua No. ....
   phone: string;
   email: string;
   postalCode: string;
